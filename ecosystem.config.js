b2e430528c59f1dbbe88c9d5ed91dc7e27a84994
Invictus Navarchus <vm045hfydfn4gqmvvfd6@protonmail.com>
2025-06-22 23:13:22 +0700

    chore: add ecosystem configuration for pm2

---
+++ ecosystem.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  apps : [{
+    name   : "NovelNotlify", // A user-friendly name for your application
+    script : "./venv/bin/python", // UPDATE WITH ABSOLUTE PATH
+    args   : "start_bot.py", // The Python script to execute
+    interpreter : "none", // PM2 should not try to interpret the script itself
+    watch  : true, // Restart the app if file changes are detected
+    ignore_watch : ["node_modules", ".git", "*.log", "venv", ".venv", "novels.db"], // Files/folders to ignore when watching
+    exp_backoff_restart_delay: 100 // Delay between restarts in milliseconds
+  }]
+};
