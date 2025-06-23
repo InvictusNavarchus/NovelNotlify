5ab712fb6bccb045f83dc9341b5d0f00683d1de0
Invictus Navarchus <vm045hfydfn4gqmvvfd6@protonmail.com>
2025-06-24 01:08:44 +0700

    fix: add missing 'novels.db-journal' to ignore_watch list

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,7 +5,7 @@ module.exports = {
     args   : "start_bot.py", // The Python script to execute
     interpreter : "none", // PM2 should not try to interpret the script itself
     watch  : true, // Restart the app if file changes are detected
-    ignore_watch : ["node_modules", ".git", "*.log", "venv", ".venv", "novels.db"], // Files/folders to ignore when watching
+    ignore_watch : ["node_modules", ".git", "*.log", "venv", ".venv", "novels.db", "novels.db-journal"], // Files/folders to ignore when watching
     exp_backoff_restart_delay: 100 // Delay between restarts in milliseconds
   }]
 };
